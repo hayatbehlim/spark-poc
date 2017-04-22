6cb8278e9e9c42fb3ef210e3e2165a6a5c3d3a19
hayat <hayat.behlim@freestoneinfotech.com>
2017-04-22 10:21:25 +0530

    Initial commit

---
+++ src/main/java/spark/poc/Runner.java
@@ -0,0 +1,40 @@
+package spark.poc;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Runner runner = new Runner();
+		runner.run();
+	}
+
+	private void run() {
+		SparkConf sparkConf = new SparkConf();
+		sparkConf.set("spark.logConf", "true");
+		sparkConf.set("spark.driver.host", "localhost");
+		SparkSession sparkSession = SparkSession.builder().master("local[*]").config(sparkConf).getOrCreate();
+		//jdbc:oracle:thin:hr/hr@//localhost:1521/JP_POC_DB
+		String jdbcUrl = "jdbc:mysql://localhost:3306/JP_POC_DB";
+		String username = "root";
+		String password = "";
+		String dbtable = "test";
+		Dataset<Row> dataset = loadDBTable(sparkSession, jdbcUrl, username, password, dbtable);
+		dataset.show(1, false);
+	}
+
+	public static Dataset<Row> loadDBTable(SparkSession sparkSession, String jdbcUrl, String username, String password,
+			String dbtable) {
+		String format = "jdbc";
+		//oracle.jdbc.driver.OracleDriver
+		String driver = "com.mysql.jdbc.Driver";
+		Dataset<Row> loadedDataset = null;
+		loadedDataset = sparkSession.read().format(format).option("url", jdbcUrl).option("dbtable", dbtable)
+				.option("user", username).option("password", password).option("driver", driver).load();
+		return loadedDataset;
+	}
+
+}
